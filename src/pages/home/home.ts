c52b66c3e692d56fcaeb832f4ed3ddd285ee7ac8
fmaldonados <felixmaldonado@unitec.edu>
2018-03-13 00:46:04 -0600

    likes casi

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController, 
+import {
+  NavController,
   AlertController, // To Add Button
   ActionSheetController // To delete
- } from 'ionic-angular';
+} from 'ionic-angular';
 
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
-import { AngularFireAuth} from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 
 
@@ -15,12 +16,13 @@ import * as firebase from 'firebase/app';
 })
 export class HomePage {
 
-  currentUser:any;
-  messagesRef:any;
+  currentUser: any;
+  messagesRef: any;
   messages: AngularFireList<any>;
-  
+  usersRef: any;
+  users: AngularFireList<any>;
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public alertCtrl: AlertController,
     public actionSheetCtrl: ActionSheetController,
     public afDatabase: AngularFireDatabase,
@@ -28,16 +30,20 @@ export class HomePage {
   ) {
     this.messagesRef = afDatabase.list('messages');
     this.messages = this.messagesRef.valueChanges();
+
+    this.usersRef = afDatabase.list('users');
+    this.users = this.usersRef.valueChanges();
+
     afAuth.authState.subscribe(user => {
       if (!user) {
         this.currentUser = null;
         return;
       }
-      this.currentUser = {uid:user.uid, photoURL: user.photoURL, name: user.displayName};
-      
+      this.currentUser = { uid: user.uid, photoURL: user.photoURL, name: user.displayName };
+
     });
   }
-  selectMessageType(){
+  selectMessageType() {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Que tipo de mensaje deseas publicar?',
       buttons: [
@@ -46,17 +52,17 @@ export class HomePage {
           handler: () => {
             this.addMessage(1);
           }
-        },{
+        }, {
           text: 'Mensaje para Amigos',
           handler: () => {
             this.addMessage(2);
           }
-        },{
+        }, {
           text: 'Mensaje Publico',
           handler: () => {
             this.addMessage(3);
           }
-        },{
+        }, {
           text: 'Cancel',
           role: 'cancel',
           handler: () => {
@@ -67,7 +73,7 @@ export class HomePage {
     });
     actionSheet.present();
   }
-  addMessage(type){
+  addMessage(type) {
     let prompt = this.alertCtrl.create({
       title: 'Crear Mensaje',
       message: "introduzca un mensaje",
@@ -89,30 +95,36 @@ export class HomePage {
           handler: data => {
             const newMessageRef = this.messagesRef.push({});
 
-            if (type==1) {
+            if (type == 1) {
               newMessageRef.set({
                 id: newMessageRef.key,
                 message: data.mensaje,
                 uid: this.currentUser.uid,
-                likes:0,
+                photo: this.currentUser.photoURL,
+                name: this.currentUser.name,
+                likes: 0,
                 type: "privado"
               });
             } else {
-              if (type==2) {
+              if (type == 2) {
                 newMessageRef.set({
                   id: newMessageRef.key,
                   message: data.mensaje,
                   uid: this.currentUser.uid,
-                  likes:0,
+                  photo: this.currentUser.photoURL,
+                  name: this.currentUser.name,
+                  likes: 0,
                   type: "amigos"
                 });
               } else {
-                if (type==3) {
+                if (type == 3) {
                   newMessageRef.set({
                     id: newMessageRef.key,
                     message: data.mensaje,
                     uid: this.currentUser.uid,
-                    likes:0,
+                    photo: this.currentUser.photoURL,
+                    name: this.currentUser.name,
+                    likes: 0,
                     type: "publico"
                   });
                 }
@@ -125,106 +137,73 @@ export class HomePage {
     prompt.present();
   }
 
-  showOptions(songId, songTitle) {
-    let actionSheet = this.actionSheetCtrl.create({
-      title: 'What do you want to do?',
-      buttons: [
-        {
-          text: 'Delete Song',
-          role: 'destructive',
-          handler: () => {
-            this.removeSong(songId);
-          }
-        },{
-          text: 'Update title',
-          handler: () => {
-            this.updateSong(songId, songTitle);
-          }
-        },{
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        }
-      ]
-    });
-    actionSheet.present();
-  }
-
-  removeSong(songId: string){
-    this.messagesRef.remove(songId);
-  }
-
-  updateSong(songId, songTitle){
-    let prompt = this.alertCtrl.create({
-      title: 'Song Name',
-      message: "Update the name for this song",
-      inputs: [
-        {
-          name: 'title',
-          placeholder: 'Title',
-          value: songTitle
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: data => {
-            this.messagesRef.update(songId, {
-              title: data.title, lastUpdatedBy: this.currentUser.uid
-            });
-          }
-        }
-      ]
-    });
-    prompt.present();
-  }
+  
 
   login() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-    .then((response)=>{
-      console.log('resultado login google:', response);
-      
-      const userRef = this.afDatabase.list('users');
+      .then((response) => {
+        console.log('resultado login google:', response);
 
-      userRef.update(response.user.uid, 
-        {
-          userId: response.user.uid, 
-          displayName: response.user.displayName,
-          photoURL: response.user.photoURL
-        });
-      //userRef.push({userId: xx.user.uid, displayName: xx.user.displayName}).then((xx)=>{
+        const userRef = this.afDatabase.list('users');
 
-      //});
-      
-    });
+        userRef.update(response.user.uid,
+          {
+            userId: response.user.uid,
+            displayName: response.user.displayName,
+            photoURL: response.user.photoURL,
+            following: [""],
+            favs:[""]
+          });
+        //userRef.push({userId: xx.user.uid, displayName: xx.user.displayName}).then((xx)=>{
+
+        //});
+
+      });
   }
 
-  
+
   logout() {
     this.afAuth.auth.signOut();
   }
 
-  getItems(ev: any) {
-    // Reset items back to all of the items
-    this.messagesRef = this.afDatabase.list('songs');
-    this.messages = this.messagesRef.valueChanges();
-    // set val to the value of the searchbar
-    let val = ev.target.value;
 
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.messagesRef = this.afDatabase.list('songs',ref => ref.orderByChild('title').startAt(val));
-      this.messages = this.messagesRef.valueChanges();
+  verMensaje(message) {
+    var render = false;
+    if (message.type == "publico") {
+      var childData
+      firebase.database().ref("/users").orderByChild("userId").equalTo(this.currentUser.uid).on("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          // key
+          var key = childSnapshot.key;
+          // value, could be object
+          childData = childSnapshot.val();
+          return true;
+          // Do what you want with these key/values here
+        });
+        if ((childData.following).indexOf(message.uid) > -1 || childData.userId == message.uid) {
+          render=true;
+        }
+      });
     }
+    return render;
   }
-
-  
+  like(message){
+    var childData
+    firebase.database().ref("/users").orderByChild("userId").equalTo(this.currentUser.uid).on("value", function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        // key
+        var key = childSnapshot.key;
+        // value, could be object
+        childData = childSnapshot.val();
+        return true;
+        // Do what you want with these key/values here
+      });
+      (childData.favs).push(message.id);
+      console.log(childData);
+      
+    });
+    message.likes+=1;
+    this.usersRef.update(childData.userId, childData);
+    this.messagesRef.update(message.id, message);
+  }
 }
